a149cc37a0022da0098280aa38514f293546baac
Dheeraj Jain <dheeraj.jain@nagarro.com>
2019-08-14 17:23:24 +0530

    first commit

---
+++ samples/PasswordFlow/AuthorizationServer/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+﻿using System.Threading.Tasks;
+using AuthorizationServer.Models;
+using AuthorizationServer.ViewModels.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationServer.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private static bool _databaseChecked;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext applicationDbContext)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            EnsureDatabaseCreated(_applicationDbContext);
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user != null)
+                {
+                    return StatusCode(StatusCodes.Status409Conflict);
+                }
+
+                user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "User");
+                    return Ok();
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed.
+            return BadRequest(ModelState);
+        }
+
+        #region Helpers
+
+        // The following code creates the database and schema if they don't exist.
+        // This is a temporary workaround since deploying database through EF migrations is
+        // not yet supported in this release.
+        // Please see this http://go.microsoft.com/fwlink/?LinkID=615859 for more information on how to do deploy the database
+        // when publishing your application.
+        private void EnsureDatabaseCreated(ApplicationDbContext context)
+        {
+            if (!_databaseChecked)
+            {
+                _databaseChecked = true;
+                context.Database.EnsureCreated();
+                string[] roleNames = { "Admin", "User", "Reporter" };
+
+                foreach (var roleName in roleNames)
+                {
+                    var roleExists = _roleManager.RoleExistsAsync(roleName).Result;
+                    if (!roleExists)
+                    {
+                        var xyz = _roleManager.CreateAsync(new IdentityRole(roleName)).Result;
+                    }
+                }
+            }
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        #endregion
+    }
+}
